1657db382408dd3731e636387550fcdea70b8939
rahulmeandev <rahulsharmainhell@gmail.com>
2017-09-04 12:00:42 +0530

    ss

--- app_server/controllers/lab-api.js
+++ app_server/controllers/lab-api.js
@@ -1,7 +1,9 @@
 var mongodb = require('mongodb').MongoClient;
 var czndb = null;
 
-var url = 'mongodb://localhost:27017/czn';
+var url = process.env.NODE_ENV ?
+  'mongodb://heroku_qzvdq0t1:8hr9ba4pik2q5njcddpej2fjgq@ds159033.mlab.com:59033/heroku_qzvdq0t1'
+   : 'mongodb://localhost:27017/czn';
 
 mongodb.connect(url, function(err, db) {
   console.log("Connected correctly to server.");
@@ -14,15 +16,17 @@ module.exports.createLab = function(req, res) {
 
 
 module.exports.getAllLabs = function(req, res) {
-  czndb.collection('labs').find(
-    {'clg': req.params.institute}
-  ).toArray(function(err, docs) {
-      res.json(docs);
+  czndb.collection('labs')
+    .find({'clg': req.params.institute})
+    .toArray(function(err, docs) {
+        res.json(docs);
   });
 }
 
 module.exports.getThisLab = function(req, res) {
-  czndb.collection('labs').find({'slug': req.params.labslug}).toArray(function(err, docs) {
+  czndb.collection('labs')
+    .find({'slug': req.params.labslug})
+    .toArray(function(err, docs) {
       res.json(docs[0]);
   });
 }
