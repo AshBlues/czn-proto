6554695a005a6c338861cd2f71b4939cb460dbe7
rahulmeandev <rahulsharmainhell@gmail.com>
2017-09-06 12:28:28 +0530

    ajax

--- app_server/controllers/lab-api.js
+++ app_server/controllers/lab-api.js
@@ -1,13 +1,11 @@
 var mongodb = require('mongodb').MongoClient;
 var czndb = null;
-const _LAB = 'labs';
+const _LAB = 'labs', _BATCH = 'batches';
 
 var url = process.env.NODE_ENV ?
   'mongodb://heroku_qzvdq0t1:8hr9ba4pik2q5njcddpej2fjgq@ds159033.mlab.com:59033/heroku_qzvdq0t1'
    : 'mongodb://localhost:27017/czn';
 
-/*var url =
-     'mongodb://heroku_qzvdq0t1:8hr9ba4pik2q5njcddpej2fjgq@ds159033.mlab.com:59033/heroku_qzvdq0t1';*/
 
 mongodb.connect(url, function(err, db) {
   console.log("Connected correctly to db server " + url);
@@ -20,7 +18,6 @@ function cln(col) {
 }
 
 module.exports.createLab = function(req, res) {
-  console.log(req.body.doc);
   var doc = JSON.parse(req.body.doc);
   cln(_LAB).insertOne(doc, function(err, status) {
           res.json({_id: doc._id});
@@ -40,5 +37,15 @@ module.exports.getThisLab = function(req, res) {
   cln(_LAB).find({'clg': req.params.institute, 'slug': req.params.labslug})
     .toArray(function(err, docs) {
       res.json(docs[0]);
+  });
+}
+
+
+
+
+module.exports.saveBatch = function(req, res) {
+  var doc = JSON.parse(req.body.doc);
+  cln(_BATCH).insertOne(doc, function(err, status) {
+          res.json({_id: doc._id});
   });
 }
--- app_server/routes/api.js
+++ app_server/routes/api.js
@@ -8,5 +8,7 @@ router.post('/:institute/labs', labCtrl.createLab);
 router.get('/:institute/labs', labCtrl.getAllLabs);
 router.get('/:institute/labs/:labslug', labCtrl.getThisLab);
 
+router.post('/:institute/labs/create-batch', labCtrl.saveBatch);
+
 
 module.exports = router;
